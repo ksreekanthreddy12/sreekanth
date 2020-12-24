7df0819291a078a2b2f45ac15893224b0d98b921
ksreekanthreddy12 <r19ci038@cit.reva.edu.in>
2020-12-24 16:54:33 +0530

    first commit

---
+++ AS1/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+	return render(request,"index.html")
+def home(request):
+	return render(request,"home.html")
+def contact(request):
+	return render(request,"contact.html")
+def about(request):
+	return render(request,"about.html")
+def services(request):
+	return render(request,"services.html")
+def register(request):
+        return render(request,"register.html")
